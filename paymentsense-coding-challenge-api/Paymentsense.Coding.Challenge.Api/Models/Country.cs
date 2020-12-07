75598458b68c9c7746af6580b270d7acb6705161
Ibrahim Oguz <ioguz@inawisdom.ccom>
2020-12-07 21:45:19 +0000

    added extra fields into models. task-5

--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Models/Country.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Models/Country.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Paymentsense.Coding.Challenge.Api.Models
 {
@@ -9,5 +6,25 @@ namespace Paymentsense.Coding.Challenge.Api.Models
     {
         public string Name { get; set; }
         public string Flag { get; set; }
+        public int Population { get; set; }
+        public List<string> Timezones { get; set; }
+        public List<Currency> Currencies { get; set; }
+        public List<Language> Languages { get; set; }
+        public string Capital { get; set; }
+    }
+
+    public class Currency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class Language
+    {
+        public string Iso639_1 { get; set; }
+        public string Iso639_2 { get; set; }
+        public string Name { get; set; }
+        public string NativeName { get; set; }
     }
 }
