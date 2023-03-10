2acc2d1639af01d652b50b198694d804bcdd1ef6
Andreas van Greunen <andreas.vangreunen@paymentsense.com>
2023-03-10 13:48:32 +0000

    Updated website to Angular 15

--- paymentsense-coding-challenge-website/src/app/app.module.ts
+++ paymentsense-coding-challenge-website/src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PaymentsenseCodingChallengeApiService } from './services';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -17,9 +16,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
   ],
-  providers: [PaymentsenseCodingChallengeApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
